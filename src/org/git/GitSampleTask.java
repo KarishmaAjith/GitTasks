c02350e3814568f6aa11922b55bcab8331e67b81
KarishmaAjith <karishmaajith23@gmail.com>
2026-05-27 01:44:44 +0530

    Added Spark in main

--- src/org/git/GitSampleTask.java
+++ src/org/git/GitSampleTask.java
@@ -1,9 +1,7 @@
 package org.git;
 
 public class GitSampleTask {
-private void task() {
-	System.out.println("Hi i am karishma");
-    System.out.println("Hadoop");
-    System.out.println("Python");
-}
+    private void task() {
+        System.out.println("Spark");
+    }
 }
