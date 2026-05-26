3e12e3349c5fda5f80a572cd29b77c58b211e0bf
KarishmaAjith <karishmaajith23@gmail.com>
2026-05-27 01:45:48 +0530

    Added Hadoop in karishma

--- src/org/git/GitSampleTask.java
+++ src/org/git/GitSampleTask.java
@@ -1,8 +1,7 @@
 package org.git;
 
 public class GitSampleTask {
-private void task() {
-	System.out.println("Hi i am karishma");
-    System.out.println("Hadoop");
-}
+    private void task() {
+        System.out.println("Hadoop");
+    }
 }
